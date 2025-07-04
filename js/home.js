f9aa7ecf23c1318b7dda61af299d1798d740c695
PTS-Studio <putilinandrej226@gmail.com>
2025-07-04 14:20:50 +0300

    version 0.3.1

--- js/home.js
+++ js/home.js
@@ -1,27 +1,27 @@
-if (navigator.userAgent.toLowerCase().indexOf('yabrowser') > -1) {
-  document.documentElement.classList.add('yandex-browser');
-}
+// if (navigator.userAgent.toLowerCase().indexOf('yabrowser') > -1) {
+//   document.documentElement.classList.add('yandex-browser');
+// }
 /*
 =============================================================
     МОДАЛЬНОЕ ОКНО
 =============================================================
 */
-let lastScrollTop = 0;
-const header = document.getElementById('header');
+// let lastScrollTop = 0;
+// const header = document.getElementById('header');
 
-window.addEventListener('scroll', function() {
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+// window.addEventListener('scroll', function() {
+//   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-  if (scrollTop > lastScrollTop && scrollTop > header.offsetHeight) {
-    // Пользователь листает вниз
-    header.classList.add('hidden');
-  } else {
-    // Пользователь листает вверх
-    header.classList.remove('hidden');
-  }
+//   if (scrollTop > lastScrollTop && scrollTop > header.offsetHeight) {
+//     // Пользователь листает вниз
+//     header.classList.add('hidden');
+//   } else {
+//     // Пользователь листает вверх
+//     header.classList.remove('hidden');
+//   }
 
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-});
+//   lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+// });
 
 const openModalButton = document.getElementById('open_modal1');
 const modal = document.getElementById('modal1');
@@ -47,14 +47,14 @@ closeModalButton2.onclick = function() {
   document.body.style.overflow = 'auto';
 };
 
-const openModalButton3 = document.getElementById('open_modal3');
-const modal3 = document.getElementById('modal_block');
-const closeModalButton3 = document.getElementById('closeModal3');
-openModalButton3.onclick = function() {
-  modal3.classList.add('show');
-  document.body.style.overflow = 'hidden';
-};
-closeModalButton3.onclick = function() {
-  modal3.classList.remove('show');
-  document.body.style.overflow = 'auto';
-};
+// const openModalButton3 = document.getElementById('open_modal3');
+// const modal3 = document.getElementById('modal_block');
+// const closeModalButton3 = document.getElementById('closeModal3');
+// openModalButton3.onclick = function() {
+//   modal3.classList.add('show');
+//   document.body.style.overflow = 'hidden';
+// };
+// closeModalButton3.onclick = function() {
+//   modal3.classList.remove('show');
+//   document.body.style.overflow = 'auto';
+// };
